86caa54589fc0f600c110e7c66c036cf38124f1a
Hanah <245273718+hanahcoquelicot@users.noreply.github.com>
2025-12-11 16:27:52 -0500

    Elevator Subsystem

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,8 +9,14 @@ public class RobotMap {
     //Drivetrain CAN id's
 
     //Other Motor Ports
+    //Elevator motor ports
+    public static final int ElevatorPort1 = 9;
+    public static final int ElevatorPort2 = 10;
 
     //Other CAN id's
+    //Elevator motor id's
+    public static final int ElevatorCanID1 = 9;
+    public static final int ElevatorCanID2 = 10;
 
     
 }
